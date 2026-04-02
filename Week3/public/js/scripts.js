ba00b2d791deb0414a0015b1461e4d948e1d30b6
Sulaiman Baig <gmssulaiman@gmail.com>
2026-04-02 16:16:51 +1100

    updating with the click me button

--- Week3/public/js/scripts.js
+++ Week3/public/js/scripts.js
@@ -1,41 +1,52 @@
 const addCards = (items) => {
+    $('#card-section').empty();
+
     items.forEach((item) => {
         let itemToAppend = `
-            <div class="col s4 center-align">
+            <div class="col s12 m6 l4">
                 <div class="card medium">
                     <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="${item.image}">
+                        <img class="activator" src="${item.image}" alt="${item.title}">
                     </div>
                     <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">${item.title}<i class="material-icons right">more_vert</i></span>
+                        <span class="card-title activator grey-text text-darken-4">
+                            ${item.title}
+                            <i class="material-icons right">more_vert</i>
+                        </span>
                         <p><a href="${item.link}">About this book</a></p>
                     </div>
                     <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">${item.title}<i class="material-icons right">close</i></span>
+                        <span class="card-title grey-text text-darken-4">
+                            ${item.title}
+                            <i class="material-icons right">close</i>
+                        </span>
                         <p>${item.description}</p>
                     </div>
                 </div>
-            </div>`;
+            </div>
+        `;
         $('#card-section').append(itemToAppend);
     });
 };
 
 const submitForm = () => {
     let formData = {};
-    formData.firstname = $('#first_name').val();
-    formData.lastname = $('#last_name').val();
+    formData.firstname = $('#firstname').val();
+    formData.lastname = $('#lastname').val();
     formData.password = $('#password').val();
     formData.email = $('#email').val();
     console.log('Form Data Submitted', formData);
 };
 
-$(document).ready(function(){
+const loadBooks = () => {
+    $.get('/api/books', function(result) {
+        addCards(result);
+    });
+};
+
+$(document).ready(function() {
     $('.materialboxed').materialbox();
     $('.modal').modal();
-    $('#clickMeButton').click(function() {
-        fetch('/api/books')
-            .then(res => res.json())
-            .then(addCards);
-    });
+    $('#loadBooksButton').click(loadBooks);
     $('#formSubmit').click(submitForm);
 });
--- Week3/server.js
+++ Week3/server.js
@@ -5,14 +5,30 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-var port = process.env.PORT || 3000;
 app.get('/api/books', function(req, res) {
     res.json([
-        { title: '1984', image: 'images/book1.jpg', link: '#!', description: 'A dystopian novel by George Orwell about surveillance.' },
-        { title: 'Pride and Prejudice', image: 'images/book2.jpg', link: '#!', description: 'Jane Austen\'s classic romance.' },
-        { title: 'To Kill a Mockingbird', image: 'images/book3.jpg', link: '#!', description: 'Harper Lee\'s story of justice and prejudice.' }
+        {
+            title: '1984',
+            image: 'images/book1.jpg',
+            link: '#!',
+            description: 'George Orwell’s dystopian novel about surveillance and control.'
+        },
+        {
+            title: 'Pride and Prejudice',
+            image: 'images/book2.jpg',
+            link: '#!',
+            description: 'Jane Austen’s classic novel about love, pride, and society.'
+        },
+        {
+            title: 'To Kill a Mockingbird',
+            image: 'images/book3.jpg',
+            link: '#!',
+            description: 'Harper Lee’s novel about justice, empathy, and prejudice.'
+        }
     ]);
 });
+
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log('App listening to port ' + port);
 });
